71080cef05ca18e02185757f404357a3c7e0aaf7
wifiextset <110332326+wifiextset@users.noreply.github.com>
2023-08-12 14:00:09 +0530

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone = $_POST["phone"];
     $model_number = $_POST["model_number"];
     
-    $to = "shubhamthakur80661@gmail.com"; // Replace with your email address
+    $to = "shubhamthakur80661@gmail.com";  // Your email address
     $subject = "Extension Setup Form Submission";
     $message = "Full Name: $full_name\n";
     $message .= "Email: $email\n";
@@ -15,7 +15,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     mail($to, $subject, $message, $headers);
     
-    header("Error Box.html"); // Redirect to Error Box.html
+    // Redirect to ThankYou.html after sending the email
+    header("Location: ThankYou.html");
     exit();
 } else {
     echo "Error: Invalid request.";
